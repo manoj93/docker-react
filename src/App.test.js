cddf47b24ac843b12cd8806a6eb92b5c988028db
Manoj Sagar <manojsagar@Manojs-MacBook-Pro-2.local>
2024-05-08 06:20:58 +0400

    new feature

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-<<<<<<< Updated upstream
-  const linkElement = screen.getByText(/learn react/i);
-=======
   const linkElement = screen.getByText(/new feature/i);
->>>>>>> Stashed changes
   expect(linkElement).toBeInTheDocument();
 });
