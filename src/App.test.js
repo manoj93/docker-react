0252751da6500f33bd582cdd6bed2b5a814ad03c
Manoj Sagar <manojsagar@Manojs-MacBook-Pro-2.local>
2024-05-08 06:17:31 +0400

    add feature text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I am a new feature/i);
   expect(linkElement).toBeInTheDocument();
 });
